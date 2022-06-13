9d324a67e0dcb99835dbfb884dd39c28d6ab5f29
Akonedev <93835972+Akonedev@users.noreply.github.com>
2022-06-14 00:10:44 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppointmentWPFDotNet6.Models
+{
+    public partial class Appointment
+    {
+        public int Id { get; set; }
+        public DateTime DateHour { get; set; }
+        public string Subject { get; set; } = null!;
+        public int IdCustomer { get; set; }
+        public int IdBroker { get; set; }
+
+        public virtual Broker IdBrokerNavigation { get; set; } = null!;
+        public virtual Customer IdCustomerNavigation { get; set; } = null!;
+    }
+}
